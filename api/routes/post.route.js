fcf7989d8f8ac8b4c8219e24a29d9a74431ba847
kal1be <yahoulbofelix@gmail.com>
2024-03-27 11:26:02 -0700

    create  posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const verifyToken = require("../utils/verifyToken")
 const router = express.Router()
-const {create} = require("../controllers/post.controllers")
+const {create,getPost} = require("../controllers/post.controllers")
 
 router.post("/create",verifyToken,create)
+router.get("/getposts",getPost)
 
 
 module.exports = router
